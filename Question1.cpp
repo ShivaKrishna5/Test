01c94ed9a137e4663947ab99ee49f58d88fcbc15
ShivaKrishna5 <72804111+ShivaKrishna5@users.noreply.github.com>
2020-10-13 18:39:38 +0530

    Update Question1.cpp

--- Question1.cpp
+++ Question1.cpp
@@ -5,19 +5,19 @@ using namespace std;
 int main()
 {
    cout << "Hello World" << endl;
-   int Level_Required=7;
-   int num_of_spaces;
-   for(int i=(-Level_Required)+1;i<=(Level_Required-1);i++)
+   int Level_Required=7; //Level required
+   int num_of_spaces;    //spaces required at each level
+   for(int i=(-Level_Required)+1;i<=(Level_Required-1);i++) //outer itteration for number of rows
    {
-       num_of_spaces=Level_Required-abs(i);
-       for(int k=1;k<=Level_Required;k++)
+       num_of_spaces=Level_Required-abs(i);                 //condition for printing space or number
+       for(int k=1;k<=Level_Required;k++)                  //inner itteration for each row
        {
            if(k<num_of_spaces)
-            cout<<" ";
-           else
-            cout<<k<<" ";
+            cout<<" ";                                  //printing space
+           else 
+            cout<<k<<" ";                               //printing number
        }
-       cout<<endl;
+       cout<<endl;                                     //printing new line after each row
    }
    
    return 0;
